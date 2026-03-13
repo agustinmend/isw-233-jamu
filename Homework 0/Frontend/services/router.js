d8c801801baeed435c9e5133b0803bbe5380bdd5
agustinmend <jose.mendoza.u1@ucb.edu.bo>
2026-03-13 14:06:29 -0400

    enrutamiento funcional

--- Homework 0/Frontend/services/router.js
+++ Homework 0/Frontend/services/router.js
@@ -1,15 +1,49 @@
 export const Router = {
     init: () => {
-        document.querySelectorAll("a.nav__link").forEach((a) => {
+        document.querySelectorAll(".Encabezado__Titulo").forEach((a) => {
             a.addEventListener("click", (event) => {
                 event.preventDefault()
-                const href = event.target.getAttribute("href")
-                Router.go(href)
+                const hash = event.target.getAttribute("href")
+                const route = Router.hashToRoute(hash)
+                Router.go(route)
             })
         })
         window.addEventListener("popstate", (event) => {
-            Router.go(event.state.route , false)
+            Router.go(event.state?.route || "/", false)
         })
         Router.go(location.pathname)
+    },
+    hashToRoute : (hash) => {
+        const routes = {
+            "#inicio" : "/",
+            "#SobreMi" : "/sobre-mi",
+            "#Habilidades" : "habilidades",
+            "#Proyectos": "/proyectos",
+            "#Blogs": "/blogs",
+            "#Contacto": "/contacto"
+        }
+        return routes[hash] || "/"
+    },
+    go : (route , addToHystory = true) => {
+        if(addToHystory) {
+            history.pushState({ route}, "", route)
+        }
+        const routeMap = {
+            "/": "inicio",
+            "/sobre-mi": "SobreMi",
+            "/habilidades": "Habilidades",
+            "/proyectos": "Proyectos",
+            "/blogs": "Blogs",
+            "/contacto": "Contacto"
+        }
+        const targetId = routeMap[route]
+        const sections = document.querySelectorAll("#inicio, #SobreMi, #Habilidades, #Proyectos, #Blogs, #Contacto")
+        sections.forEach(section => {
+            section.style.display = "none"
+        })
+        if(targetId) {
+            document.getElementById(targetId).style.display = "block"
+        }
+        window.scrollTo(0,0)
     }
 }
