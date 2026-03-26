50ffb0f81f7462c9dd1685640dacf9a40f7be916
agustinmend <jose.mendoza.u1@ucb.edu.bo>
2026-03-25 23:09:02 -0400

    Solucion inconsistencia con nombre de archivos en github

--- Homework 0/Frontend/src/main.js
+++ Homework 0/Frontend/src/main.js
@@ -5,7 +5,16 @@ import './components/habilidades/habilidades.js'
 import './components/inicio/inicio.js'
 import './components/pie-de-pagina/pie-de-pagina.js'
 import './components/proyectos/proyectos.js'
-import './components/sobreMi/sobre-mi.js'
+import './components/sobre-mi/sobre-mi.js'
+import './components/blog/blog.css'
+import './components/contacto/contacto.css'
+import './components/encabezado/encabezado.css'
+import './components/habilidades/habilidades.css'
+import './components/inicio/inicio.css'
+import './components/page/page.css'
+import './components/pie-de-pagina.css'
+import './components/proyectos/proyectos.css'
+import './components/sobre-mi/sobre-mi.css'
 import { Router } from "./services/router.js";
 
 document.addEventListener('DOMContentLoaded', () => {
